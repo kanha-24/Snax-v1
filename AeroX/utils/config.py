ff0066daf7c331151f187ab170875045718e638a
GotheX <shubhamhande204@gmail.com>
2026-09-08 20:02:07 +0530

    Update config.py

--- AeroX/utils/config.py
+++ AeroX/utils/config.py
@@ -1,12 +1,12 @@
 import os 
 
 TOKEN =os .environ .get ("TOKEN")
-NAME ="Strelizia-bot"
-server ="https://discord.gg/JxCFmz9nZP"
-ch ="https://discord.com/channels/1324668335069331477/1324668336470102143"
-OWNER_IDS =[1124248109472550993 ,1366749025814052895 ,1198187445859139595 ]
-BotName ="Strelizia-bot"
-serverLink ="https://discord.gg/JxCFmz9nZP"
+NAME ="Snax"
+server ="https://discord.gg/safarr"
+ch ="https://discord.com/channels/1522251322949238957/1546472932153495572"
+OWNER_IDS =[1338300439216525364, 1501844549754949726]
+BotName ="Snax"
+serverLink ="https://discord.gg/safarr"
 """
 : ! Aegis !
     + Discord: root.exe
